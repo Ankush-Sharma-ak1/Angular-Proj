42465d8e51f0f46d22ecb209c695f2c78bba9a51
Ankush-Sharma-ak1 <ankush.sharma@catalystone.com>
2020-10-09 10:28:01 +0530

    prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ firebaseConfig: {
+  apiKey: "AIzaSyAox4vw5e4uY5zkcVtdV2EQ28fxzAIjJFg",
+  authDomain: "employee-data-51f8c.firebaseapp.com",
+  databaseURL: "https://employee-data-51f8c.firebaseio.com",
+  projectId: "employee-data-51f8c",
+  storageBucket: "employee-data-51f8c.appspot.com",
+  messagingSenderId: "599301184293",
+  appId: "1:599301184293:web:6f220aab5186163b0418cc",
+  measurementId: "G-B8NC25Y52H"
+   }
 };
